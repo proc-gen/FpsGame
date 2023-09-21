2d2cc04456b3b81b23b908b431d21873a32c093a
Micah Caunter <65076703+proc-gen@users.noreply.github.com>
2023-09-21 17:23:24 -0400

    Data files (#6)
    * feat: add json file manager
    
    * feat: add load/save of settings screen and apply on game load
    
    * feat: create constants for game files

---
+++ FpsGame.Common/Utils/JsonFileManager.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FpsGame.Common.Utils
+{
+    public static class JsonFileManager
+    {
+        public static T LoadFile<T>(string path, bool createIfNotExist = false)
+            where T : new()
+        {
+            if(!File.Exists(path) && createIfNotExist)
+            {
+                SaveFile(new T(), path);
+            }
+
+            var data = JObject.Parse(File.ReadAllText(path));
+            return data.ToObject<T>();
+        }
+
+        public static async Task<T> LoadFileAsync<T>(string path, bool createIfNotExist = false)
+            where T : new()
+        {
+            if (!File.Exists(path) && createIfNotExist)
+            {
+                SaveFile(new T(), path);
+            }
+
+            var data = JObject.Parse(await File.ReadAllTextAsync(path));
+            return data.ToObject<T>();
+        }
+
+        public static void SaveFile<T>(T data, string path)
+            where T : new()
+        {
+            var jObject = JObject.FromObject(data);
+            File.WriteAllText(path, jObject.ToString());
+        }
+
+        public static async Task SaveFileAsync<T>(T data, string path)
+            where T : new()
+        {
+            var jObject = JObject.FromObject(data);
+            await File.WriteAllTextAsync(path, jObject.ToString());
+        }
+    }
+}
---
+++ FpsGame/Containers/SettingsContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FpsGame.Containers
+{
+    public class SettingsContainer
+    {
+        public string Resolution { get; set; }
+        public string WindowMode { get; set; }
+    }
+}
